44d025941d6207a8bcfbfb25cd71823e5caff1db
sandeep <sandeep@kali-laptop>
2024-08-27 23:27:30 +0530

    modifing terraform files like removing CDN

--- tf_infrastructure/main.tf
+++ tf_infrastructure/main.tf
@@ -1,16 +1,30 @@
 provider "aws" {
-  region = "us-east-1"  # Adjust as needed
+  region = "us-east-1" # Adjust as needed
 }
 
 # S3 Bucket for Static Website Hosting
 resource "aws_s3_bucket" "website" {
-  bucket = "my-app-static-website"  # Change to a unique name
-  acl    = "public-read"
-
-  website {
-    index_document = "index.html"
-    error_document = "404.html"
+  bucket = "my-app-static-website" # Change to a unique name
+  tags = {
+    Name = "my-app-static-website"
   }
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "404.html"
+  }
+}
+
+resource "aws_s3_bucket_acl" "website_public_access" {
+  bucket = aws_s3_bucket.website.id
+  acl    = "public-read"
 }
 
 resource "aws_s3_bucket_policy" "website_policy" {
@@ -20,58 +34,44 @@ resource "aws_s3_bucket_policy" "website_policy" {
 
 data "aws_iam_policy_document" "website_policy" {
   statement {
-    actions = ["s3:GetObject"]
+    actions   = ["s3:GetObject"]
     resources = ["${aws_s3_bucket.website.arn}/*"]
-    effect = "Allow"
+    effect    = "Allow"
     principals {
-      type = "AWS"
+      type        = "AWS"
       identifiers = ["*"]
     }
   }
 }
 
-# CloudFront Distribution
-resource "aws_cloudfront_distribution" "cdn" {
-  origin {
-    domain_name = aws_s3_bucket.website.bucket_regional_domain_name
-    origin_id   = "s3-origin"
+# # CloudFront Distribution
+# resource "aws_cloudfront_distribution" "cdn" {
+#   origin {
+#     domain_name = aws_s3_bucket.website.bucket_regional_domain_name
+#     origin_id   = "s3-origin"
+#     s3_origin_config {
+#       origin_access_identity = aws_cloudfront_origin_access_identity.cdn.iam_arn
+#     }
+#   }
 
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.cdn.cloudfront_origin_access_identity_path
-    }
-  }
+#   enabled             = true
+#   is_ipv6_enabled     = true
+#   default_root_object = "index.html"
 
-  enabled             = true
-  is_ipv6_enabled      = true
-  default_root_object = "index.html"
+#   default_cache_behavior {
+#     target_origin_id       = "s3-origin"
+#     viewer_protocol_policy = "redirect-to-https"
 
-  default_cache_behavior {
-    target_origin_id       = "s3-origin"
-    viewer_protocol_policy = "redirect-to-https"
+#     cached_methods = ["GET", "HEAD"]
+#   }
 
-    allowed_headers = ["*"]
-    allowed_methods = ["GET", "HEAD"]
-    allowed_origins = ["*"]
-    cached_methods  = ["GET", "HEAD"]
+#   price_class = "PriceClass_100"
 
-    forward_cookie {
-      forward = "none"
-    }
+#   viewer_certificate {
+#     cloudfront_default_certificate = true
+#   }
 
-    forward_query_string = false
-  }
-
-  price_class = "PriceClass_100"
-
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
-
-  tags = {
-    Name = "my-app-cdn"
-  }
-}
-
-resource "aws_cloudfront_origin_access_identity" "cdn" {
-  comment = "Origin Access Identity for my app"
-}
+#   tags = {
+#     Name = "my-app-cdn"
+#   }
+# }
