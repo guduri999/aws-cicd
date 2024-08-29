558c33bad497241123853128efe937d366b173ce
sandeep <sandeep@kali-laptop>
2024-08-29 21:57:29 +0530

    testing plan

--- tf_infrastructure/output.tf
+++ tf_infrastructure/output.tf
@@ -1,16 +1,16 @@
-# output "cloudfront_domain_name" {
-#   value = aws_cloudfront_distribution.cdn.domain_name
-# }
+# # output "cloudfront_domain_name" {
+# #   value = aws_cloudfront_distribution.cdn.domain_name
+# # }
+
+# # output "s3_bucket_name" {
+# #   value = aws_s3_bucket.website.bucket
+# # }
+
+
+# # output "cloudfront_domain_name" {
+# #   value = aws_cloudfront_distribution.cdn.domain_name
+# # }
 
 # output "s3_bucket_name" {
 #   value = aws_s3_bucket.website.bucket
 # }
-
-
-# output "cloudfront_domain_name" {
-#   value = aws_cloudfront_distribution.cdn.domain_name
-# }
-
-output "s3_bucket_name" {
-  value = aws_s3_bucket.website.bucket
-}
